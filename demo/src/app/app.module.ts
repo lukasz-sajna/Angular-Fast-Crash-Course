00b9b6e02b5ceca147f07a5e4c38710723ce67bc
Łukasz Sajna <lukasz.sajna@gmail.com>
2019-04-30 19:27:14 +0200

    Chapter 6 Completed

--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -1,19 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
+const appRoutes: Routes =
+  [
+    { path: '', component: HomeComponent },
+    { path: 'contact', component: ContactComponent },
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
